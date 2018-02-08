5773b4931d2a427ea83bf69e3bca4e304b13c544
Arjan Scherpenisse <arjan@scherpenisse.net>
2018-02-08 15:46:18 +0100

    New botsi

--- mode/botsi.js
+++ mode/botsi.js
@@ -26,12 +26,12 @@ ace.define("ace/mode/botsi_highlight_rules",["require","exports","module","ace/l
     var BotsiHighlightRules = function() {
 
       var buildinConstants = (
-        "event|postback|web|image|location|audio|video|expecting|class|trigger|random|subtitle|image_url|default_action|webview_height_ratio|messenger_extensions|fallback_url|webview_share_button|url|open"
+        "event|postback|web|image|location|audio|video|expecting|quick_replies|class|trigger|random|subtitle|image_url|default_action|webview_height_ratio|messenger_extensions|fallback_url|webview_share_button|url|open|intent|entity|match|learn|label"
           + "|query|reply|chat_link|oauth_link|get_full_token|get_token_info|get_access_token"
         );
 
         var builtinFunctions = (
-          "if|else|branch|do|end|import|repeat|in|dialog|task|elixir|def|defp|say|ask|emit|log|invoke|perform|stop|show|http_post|http_get|http_put|http_patch|http_delete|sleep|think|remember|forget|tag|untag|template|buttons"
+          "if|else|branch|do|end|import|repeat|in|dialog|task|elixir|def|defp|say|ask|emit|log|invoke|perform|stop|show|http_post|http_get|http_put|http_patch|http_delete|pause|type|remember|forget|tag|untag|template|buttons"
           + "|mail|cancel_emit"
         );
 
